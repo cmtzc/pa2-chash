79d00bedd96bbf724bacf02b55e6e2f18f0def86
cmtzc <cmartinezrx7@gmail.com>
2025-03-31 15:53:46 -0400

    Update crud

--- crud.c
+++ crud.c
@@ -1,6 +1,31 @@
+#include <string.h>
 #include "crud.h"
 
-void insert(char *key, int values)
+uint32_t jenkins_one_at_a_time_hash(const char *key)
 {
-
+    size_t len = strlen(key);
+    size_t i = 0;
+    uint32_t hash = 0;
+    while (i != len)
+    {
+        hash += key[i++];
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
 }
+
+void insert(const char *name, uint32_t salary)
+{
+}
+
+extern void delete(const char *name)
+{
+}
+
+// extern hashRecord *search(const char *name)
+// {
+// }
--- crud.h
+++ crud.h
@@ -9,4 +9,17 @@ typedef struct hash_struct
     struct hash_struct *next;
 } hashRecord;
 
-extern void insert(char *key, int values);
+typedef struct hashMap
+{
+    int numElements, capacity;
+    hashRecord **arr;
+} hashTable;
+
+extern uint32_t jenkins_one_at_a_time_hash(const char *key);
+
+extern void insert(const char *name, uint32_t salary);
+
+extern void delete(const char *name);
+
+extern hashRecord* search(const char *name);
+
