7466e8832f58929135a1a723c2151df71a258088
cmtzc <cmartinezrx7@gmail.com>
2025-03-31 15:53:57 -0400

    Update chash

--- chash.c
+++ chash.c
@@ -10,11 +10,24 @@ int main(void)
     FILE *fp = fopen("commands.txt", "r");
     assert(fp != 0);
 
-    char *plain = getLetters(fp, getFileSize(fp));
-    printText(plain);
+    char output[] = "output.txt";
+    FILE *op = fopen(output, "w");
+    assert(op != 0);
+
+    char command[50];
+    char line[50];
+    int threadCount, salary;
+
+    fgets(line, sizeof(line), fp);
+    assert(line != NULL);
+    if (sscanf(line, "%49[^,], %d, %d", command, &threadCount, &salary) == 3)
+        fprintf(op, "Time: %lld\nCommand: %s\nThread Count: %d\nSalary: %d\n", getTime(), command, threadCount, salary);
+
+    // char *plain = getLetters(fp, getFileSize(fp));
+    // printText(plain, op);
 
 
     fclose(fp);
-
+    fclose(op);
     return EXIT_SUCCESS;
 }
